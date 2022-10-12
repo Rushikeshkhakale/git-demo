0ad6e15756cab7974ca5f0494138cb958e3a51cb
Rushikeshkhakale <rushikeshkhakale@gmail.com>
2022-10-12 21:07:58 +0530

    first commit

---
+++ ArraylistDemo/src/Demo/ArraylistDemo.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import java.util.ArrayList;
+
+public class ArraylistDemo {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list=new ArrayList<>();
+		list.add(12);
+		list.add(23);
+		System.out.println(list);
+	}
+}
